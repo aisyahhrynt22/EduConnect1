bf3e85ca68438c174023e3c9fb087d2e564a955a
muhammadmubarokazzam <mmazzam4@gmail.com>
2023-06-25 17:17:43 +0700

    edit sidebar

--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -5,7 +5,7 @@
       <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a> --}}
-    <a href="../../index3.html" class="brand-link">
+    <a class="brand-link">
         <img src="{{asset('admin/dist/img/logouai.png')}}" alt="AdminLTE Logo" class="brand-image img-rectangle elevation-3" style="opacity: .9">
         <span class="brand-text font-weight-light">Ainut</span>
       </a>
