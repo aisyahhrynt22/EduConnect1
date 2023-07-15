d4389974d2ea44b225a806144497546576578388
ainuttoriqun <ainuttoriqunnajah@if.uai.ac.id>
2023-07-15 20:03:58 +0700

    tunggureg nuti

---
+++ resources/views/Agen/tunggureg.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>tunggureg</title>
+</head>
+<body>
+  <h1>menunggu approve dari admin</h1>
+</body>
+</html>
