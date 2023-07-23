d9886c92231af50717b82f2cbd644b5b1fbe3675
HaikalPriyarachman <m.haikal@if.uai.ac.id>
2023-07-23 21:36:39 +0700

    merapihkan front-end regis agen

--- resources/views/Agen/regis.blade.php
+++ resources/views/Agen/regis.blade.php
@@ -32,7 +32,27 @@
 
     <!-- CDN -->
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script> --}}
+    <style>
+        .example {
+            background-color: #eee;
 
+            overflow-y: scroll;
+            /* Add the ability to scroll */
+        }
+
+        /* Hide scrollbar for Chrome, Safari and Opera */
+        .example::-webkit-scrollbar {
+            display: none;
+        }
+
+        /* Hide scrollbar for IE, Edge and Firefox */
+        .example {
+            -ms-overflow-style: none;
+            /* IE and Edge */
+            scrollbar-width: none;
+            /* Firefox */
+        }
+    </style>
 </head>
 
 <body>
@@ -42,133 +62,148 @@
     <!-- Wrapper -->
     <div class="login-wrapper columns is-gapless">
         <!-- Form section -->
-        <div class="column is-7">
-            <div class="hero is-fullheight">
-                <div class="hero-heading">
-                    <div class="auth-logo mt-30 has-text-centered">
-                        <a href="/"><img class="dark-logo switcher-logo" src="https://penerimaan.uai.ac.id/file/images/logouai.png" alt="" width="180"></a>
+
+        <div class="column is-7 overflow-auto">
+            <div style="overflow: scroll;height:749px" class="example">
+                <div class="hero">
+                    <div class="hero-heading">
+                        <div class="auth-logo mt-30 has-text-centered">
+                            <a href="/"><img class="dark-logo switcher-logo" src="https://penerimaan.uai.ac.id/file/images/logouai.png" alt="" width="180"></a>
+                        </div>
                     </div>
-                </div>
-                <div class="hero-body mb-80 ">
-                    <div class="container">
+                    <div class="hero-body mb-80 ">
+                        <div class="container">
 
 
-                        <div class="columns is-vcentered">
-                            <div class="column is-9 is-offset-1">
-                                <!-- <div class="flex-card light-bordered light-raised">
+                            <div class="columns is-vcentered">
+                                <div class="column is-9 is-offset-1">
+                                    <!-- <div class="flex-card light-bordered light-raised">
                                     <div class="card-body"> -->
-                                <div class="auth-content has-text-centered">
-                                    <h3 class="title is-3 is-styled">Registrasi Agen </h3>
-                                    <p class="title">Daftar akun agen anda sekarang!</p>
-                                    <!-- <hr> -->
-                                </div>
-                                <form id="signupform">
-                                    {{-- <input  type="hidden" id="jenjang" name="jenjang" value="Kelas Karyawan"/> --}}
+                                    <div class="auth-content has-text-centered">
+                                        <h3 class="title is-3 is-styled">Registrasi Agen </h3>
+                                        <p class="title">Daftar akun agen anda sekarang!</p>
+                                        <!-- <hr> -->
+                                    </div>
+                                    <form id="signupform">
+                                        {{-- <input  type="hidden" id="jenjang" name="jenjang" value="Kelas Karyawan"/> --}}
 
-                                    <div class="columns pt-8">
-                                        <div class="column" style="display: flex;">
-                                            <div class="field">
+                                        <!-- <div class="columns pt-8"> -->
+                                        <div class="row mt-2">
+                                            <div class="col">
                                                 <div class="control">
                                                     <label>First name <span style='font-size:12px;'></span> </label>
                                                     <input class="input is-medium mt-3" type="text" id="nama_lengkap" name="nama_lengkap" tabindex="1" />
                                                 </div>
                                             </div>
-                                            <div class="field" style="margin-left: 10px;">
+                                            <div class="col">
                                                 <div class="control">
                                                     <label>Last name <span style='font-size:12px;'></span> </label>
                                                     <input class="input is-medium mt-3" type="email" id="email" name="email" tabindex="2" />
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                        <!-- </div> -->
 
-                                    <div class="columns pt-8">
-                                        <div class="column" style="display: flex;">
-                                            <div class="field">
+                                        <!-- <div class="columns pt-8"> -->
+                                        <div class="row mt-2">
+                                            <div class="col">
                                                 <div class="control">
                                                     <label>NIK <span style='font-size:12px;'>(Sesuai KTP)</span> </label>
                                                     <input class="input is-medium mt-3" type="text" id="nama_lengkap" name="nama_lengkap" tabindex="1" />
                                                 </div>
                                             </div>
-                                            <div class="field" style="margin-left: 10px;">
+                                            <div class="col">
                                                 <div class="control">
                                                     <label>Email <span style='font-size:12px;'>(Gunakan Email Aktif)</span> </label>
                                                     <input class="input is-medium mt-3" type="email" id="email" name="email" tabindex="2" />
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                        <!-- </div> -->
 
-                                    <div class="columns pt-8">
-                                        <div class="column" style="display: flex;">
-                                            <div class="field">
+                                        <!-- <div class="columns pt-8"> -->
+                                        <div class="row mt-2">
+                                            <div class="col">
                                                 <div class="control">
                                                     <label>Username <span style='font-size:12px;'></span> </label>
                                                     <input class="input is-medium mt-3" type="text" id="nama_lengkap" name="nama_lengkap" tabindex="1" />
                                                 </div>
                                             </div>
-                                            <div class="field" style="margin-left: 10px;">
+                                            <div class="col">
                                                 <div class="control">
                                                     <label>Date of birth <span style='font-size:12px;'></span> </label>
                                                     <input class="input is-medium mt-3" type="email" id="email" name="email" tabindex="2" />
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                        <!-- </div> -->
 
-                                    <div class="columns pt-8">
-                                        <div class="column" style="display: flex;">
-                                            <div class="field">
+                                        <!-- <div class="columns pt-8"> -->
+                                        <div class="row mt-2">
+                                            <div class="col">
                                                 <div class="control">
                                                     <label>Password</label>
                                                     <input class="input is-medium mt-3" type="password" id="password" name="password" tabindex="3" />
                                                 </div>
                                             </div>
-                                            <div class="field" style="margin-left: 10px;">
+                                            <div class="col">
                                                 <div class="control">
                                                     <label>Confirm Password</label>
                                                     <input class="input is-medium mt-3" type="password" id="repassword" name="repassword" tabindex="4" />
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-
-                                    <div class="columns pt-8">
-                                        <div class="column" style="display: flex;">
-                                            <div class="field">
+                                        <!-- </div> -->
+                                        <div class="row mt-2">
+                                            <div class="col">
+                                                <div class="">
+                                                    <input type="checkbox" aria-label="Checkbox for following text input">
+                                                    <span class="pl-2">Dosen, Karyawan, Umum </span>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="">
+                                                    <input type="checkbox" aria-label="Checkbox for following text input">
+                                                    <span class="pl-2">Mahasiswa/i </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- <div class="columns pt-8"> -->
+                                        <div class="mt-2">
+                                            <div class="">
                                                 <div class="control">
                                                     <label>Phone number <span style='font-size:12px;'></span> </label>
                                                     <input class="input is-medium mt-3" type="text" id="nama_lengkap" name="nama_lengkap" tabindex="1" />
                                                 </div>
                                             </div>
-                                            <div class="field" style="margin-left: 10px;">
+                                            <div class="mt-2">
                                                 <div class="control">
                                                     <label>Address <span style='font-size:12px;'></span> </label>
-                                                    <input class="input is-medium mt-3" type="email" id="email" name="email" tabindex="2" />
+                                                    <textarea class="textarea is-medium mt-3" type="text" id="text" name="text" tabindex="2"> </textarea>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                        <!-- </div> -->
 
-                                    <div class="has-text-right">
-                                        <!-- <button class="button is-light button-cta is-bold no-lh">
+                                        <div class="has-text-right">
+                                            <!-- <button class="button is-light button-cta is-bold no-lh">
                                                     Cancel
                                                 </button> -->
 
-                                        {{-- Terms and conditions --}}
-                                        <div class="form-group">
-                                            <div class="form-check">
-                                                <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
-                                                <label class="form-check-label" for="invalidCheck3">
-                                                    Agree to <a href="https://drive.google.com/file/d/1msXCRdxfnP4Ljx47XBYyJPSFFoCD-8Vs/view?usp=sharing" style="color: rgb(255, 0, 0);">terms and conditions</a>
-                                                </label>
-                                                <div class="invalid-feedback">
-                                                    You must agree before submitting.
+                                            {{-- Terms and conditions --}}
+                                            <div class="form-group">
+                                                <div class="form-check">
+                                                    <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
+                                                    <label class="form-check-label" for="invalidCheck3">
+                                                        Agree to <a href="https://drive.google.com/file/d/1msXCRdxfnP4Ljx47XBYyJPSFFoCD-8Vs/view?usp=sharing" style="color: rgb(255, 0, 0);">terms and conditions</a>
+                                                    </label>
+                                                    <div class="invalid-feedback">
+                                                        You must agree before submitting.
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
 
-                                        <button tabindex="7" type="submit" id="daftarBtn" class="
+                                            <button tabindex="7" type="submit" id="daftarBtn" class="
                                                     button
                                                     btn-align
                                                     button-cta
@@ -177,18 +212,19 @@
                                                     raised
                                                     primary-btn
                                                     ">
-                                            Sign Up
-                                        </button>
-                                    </div>
-                                </form>
-                                <!-- <p>Ada Pertanyaan ? <a href="#">Klik disini.</a></p>
+                                                Sign Up
+                                            </button>
+                                        </div>
+                                    </form>
+                                    <!-- <p>Ada Pertanyaan ? <a href="#">Klik disini.</a></p>
                                         <p>Lupa Password ? <a href="https://penerimaan.uai.ac.id/login/lupa_password">Klik disini.</a></p> -->
-                                <!-- </div>
+                                    <!-- </div>
                                 </div> -->
+                                </div>
                             </div>
+
+
                         </div>
-
-
                     </div>
                 </div>
             </div>
